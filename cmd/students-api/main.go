6aa693270ced8898f534c8c3f7d27d1fcfb8956f
Bedant Chhetri <bedantchhetri7@gmail.com>
2026-05-12 18:20:30 +0000

    implement HTTP server with routing and configuration loading

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/bedantchh/students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to students api")
+	cfg := config.MustLoad()
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students api"))
+	})
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Println("Server started")
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Failed to start server")
+	}
 }
