23742bf1ad75b4c80dd6835d391772b80a58da34
Bedant Chhetri <bedantchhetri7@gmail.com>
2026-05-16 12:51:20 +0000

    add student creation handler and validation logic

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -10,11 +10,13 @@ import (
 
 	"github.com/bedantchh/students-api/internal/types"
 	"github.com/bedantchh/students-api/internal/utils/response"
+	"github.com/go-playground/validator/v10"
 )
 
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
+		slog.Info("creating a student")
 		var student types.Student
 
 		err := json.NewDecoder(r.Body).Decode(&student)
@@ -23,7 +25,17 @@ func New() http.HandlerFunc {
 			return
 		}
 
-		slog.Info("creating a student")
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+
+		//request validation
+		if err := validator.New().Struct(student); err != nil {
+			validateErrs := err.(validator.ValidationErrors)
+			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validateErrs))
+			return
+		}
 
 		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "OK"})
 	}
